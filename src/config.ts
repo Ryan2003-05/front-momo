3855f1a16f109282c5fa3ad1eeeeebbf7cbd0d24
Ryan2003-05 <ornelick2@gmail.com>
2026-06-02 19:47:44 +0200

    fix react router on vercel

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+console.log("API_URL =", import.meta.env.VITE_API_URL);
+console.log("FRONT_URL =", import.meta.env.VITE_FRONT_URL);
+
 export const API_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000/api";
 export const FRONT_URL = import.meta.env.VITE_FRONT_URL ?? window.location.origin;
 
